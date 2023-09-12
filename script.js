e69544fe74f890ed8868425b339640bbc95c9b10
MdNazmulHaqueA <naz089fvrict@gmail.com>
2023-09-12 06:15:06 +0600

    Popular meal section added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// THis code will work one time. After that Uncaught TypeError: Cannot read properties of null (reading 'classList')
+
+// const icons = document.querySelectorAll(".section-1-icons i");
+// setInterval(() => {
+//   const icon = document.querySelector(".section-1-icons .change");
+//   icon.classList.remove("change");
+//   icon.nextElementSibling.classList.add("change");
+// }, 2000);
+
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 setInterval(() => {

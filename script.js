fc315521eec4e3d10314598f6cb019ca3e61f540
MdNazmulHaqueA <naz089fvrict@gmail.com>
2023-09-17 18:01:19 +0600

    added nav bar and footer

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.querySelector('.menu').addEventListener('click',()=>{
+  /*
+we need to select the navbar and the menu icon.
+Actually, we can do that separately, but would be better if we assign to both elements the same class and then select both of them simultaneously. So let's assign to them class 'target' in the html.
+
+ - We need to loop through the node list which is returned by the query selector all method and we have to add to each element a new class using toggle method.
+
+- After that we will define new styles using this class from CSS.
+
+- So in order to loop through the node list, I'm going to use one of the array helper methods called forEach.
+
+- Actually forEach method takes one argument, which is a callback function and this function will be executed for each item in the node list.
+*/
+  document.querySelectorAll('.target').forEach((item)=>{
+    item.classList.toggle('change');
+  })
+})
+
+
+
+
 // THis code will work one time. After that Uncaught TypeError: Cannot read properties of null (reading 'classList')
 
 // const icons = document.querySelectorAll(".section-1-icons i");
